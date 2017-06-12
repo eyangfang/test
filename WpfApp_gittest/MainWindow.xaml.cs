b081ff6295000b9028b505bc6df0613fafa8f00f
YangFang86 <yangfang86@outlook.com>
2017-06-12 16:18:29 +0800

    add click event

--- WpfApp_gittest/MainWindow.xaml.cs
+++ WpfApp_gittest/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp_gittest
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
